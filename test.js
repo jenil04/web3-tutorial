c77a7787b64c5dabe2f47a6f049b4464b787ecdd
Jenil Thakker <jenilkaushik.thakker@sjsu.edu>
2019-09-01 23:07:31 -0700

    finalizing tutorial

--- test.js
+++ test.js
@@ -1,31 +1,40 @@
-var assert = require('assert')
+const assert = require('assert');
+const Modules = require('./modules.js');
 
-describe('Web3 stage tests', function() {
-    describe('Test web3 functions', () => {
-        it('Should return account balance', () => {
-            web3.eth.getBalance(account, (err, wei) => {
-                balance = web3.utils.fromWei(wei, "ether");
-                assert(balance >= 0);
-              });
-        });
-        it('should return seller address', async () => {
-            
-        });
-    
-        it('should return transaction information', async () => {
-            
-        });
-    
-        it('should return keccak256 hash', async () => {
-            
-        });
-    
-        it('should return random hex', async () => {
-            
-        });
-    
-        it('should return block number', async () => {
-            
-        });
-     });    
-  });
+let modules;
+describe('Web3 Stage Tests', function() {
+    before(() => {
+        modules = new Modules();
+    });
+    describe('Testing web3 functions', function() {
+        it('balance should be more than or equal to 0', function() {
+            let balance  = modules.getAccountInfo();
+            assert(balance >= 0);
+        })
+
+        it('seller should be set to an 0x address', function() {
+            let seller  = modules.getContractInfo();
+            assert.equal(seller, 0x0000000000000000000000000000000000000000);
+        })
+
+        it('Testing for transaction info', function() {
+            let tx  = modules.getTxInfo();
+            assert.ok(tx);
+        })
+
+        it('Testing the hash function', function() {
+            let hash  = modules.hashStr();
+            assert.equal(hash, 0x9159e86ab404edde107a2ddf13fb802ba9049e48fd10e3eba0238273972cbf6d);
+        })
+
+        it('Testing the random hex function', function() {
+            let hex  = modules.randomHexNum();
+            assert.ok(hex);
+        })
+
+        it('Testing for the getBlockNum function', function() {
+            let blk  = modules.getBlockNum();
+            assert.ok(blk);
+        })
+    })
+})
